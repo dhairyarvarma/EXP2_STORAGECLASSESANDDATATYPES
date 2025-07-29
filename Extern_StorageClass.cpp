ffc21c79a758f2ff5db01f0ec4fb13296a9e29f3
dhairyarvarma <dhairyarajvarma@gmail.com>
2025-07-30 00:17:33 +0530

    Create Extern_StorageClass.cpp

---
+++ Extern_StorageClass.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int a = 5;
+
+void func() {
+    extern int a;
+    cout << "Address of func variable a: " << &a << endl;
+    a++;
+    cout << "Size of variable: " << sizeof(a) << endl;
+    cout << "The variable is :" << a << endl;
+    cout << endl;
+}
+
+int main() {
+    func();
+    func();
+    func();
+    cout << "The variable is :" << a << endl;
+    cout << "Address of global variable a: " << &a;
+    return 0;
+}
