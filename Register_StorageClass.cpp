a32f358389e9050f1100e9eaa953ae154743ab6b
dhairyarvarma <dhairyarajvarma@gmail.com>
2025-07-30 00:18:01 +0530

    Create Register_StorageClass.cpp

---
+++ Register_StorageClass.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int a = 5;
+
+void func() {
+    register int a = 0;
+    a++;
+    cout << "Size of variable: " << sizeof(a) << endl;
+    cout << "The variable is :" << a << endl;
+    cout << endl;
+}
+
+int main() {
+    func();
+    func();
+    func();
+    cout << "The variable is :" << a << endl;
+    cout << "Address of global variable a: " << &a;
+    return 0;
+}
